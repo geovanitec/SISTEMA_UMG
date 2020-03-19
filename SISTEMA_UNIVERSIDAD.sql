d595640abbcb0c48c11cc8d556ae9ea667ccd768
angelchaco <availc@miumg.edu.gt>
2020-03-19 23:09:07 +0100

    SE AGREGO LA TABLA CURSOS Y SEDE

--- SISTEMA_UNIVERSIDAD.sql
+++ SISTEMA_UNIVERSIDAD.sql
@@ -1,6 +1,14 @@
 create database SISTEMA_UMG;
 use SISTEMA_UMG;
 
+create table CURSOS 
+(
+
+CodigoCurso int primary Key,
+NombreCurso varchar(70) not null
+
+)Engine = InnoDB Default charset = Latin1;
+
 create table BOLETAS(
 NumeroBoleta int primary key,
 CodigoAlumno int,
@@ -42,15 +50,29 @@ create table CATEDRATICOS_SEDE
 CodigoCatedratico int primary key,
 NombreCatedratico varchar (70) not null,
 CursoCatedratico varchar (70) not null,
-CorreoCatedratico varchar (100) not null
+CorreoCatedratico varchar (100) not null,
+CodigoCurso int not null,
+foreign key (CodigoCurso) references CURSOS(CodigoCurso),
 )Engine = InnoDB Default charset = Latin1 ;
 
+create table SEDE
+(
+
+CodigoSede int primary key,
+NombreSede varchar (70) not null,
+DireccionSede varchar (100) not null
+
+)Engine = InnoDB Default charset = Latin1 ;
 
 
 create table FACULTADES
 (
 CodigoFacultad int primary key,
-NombreFacultad varchar (70)not null
+NombreFacultad varchar (70)not null,
+CodigoSede int not null,
+CodigoCurso int not null,
+foreign key (CodigoSede) references SEDE(CodigoSede),
+foreign key (CodigoCurso) references CURSOS(CodigoCurso)
 )Engine = InnoDB Default charset = Latin1 ;
 
 
@@ -59,5 +81,3 @@ create table CONTROL_CATEDRATICO
 foreign key(NumeroCarnet)references ALUMNOS (NumeroCarnet)
 
 )
-
-
