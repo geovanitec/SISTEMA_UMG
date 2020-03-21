96aff247268ff0db798ae6a126238b4477442013
geovanitec <60049761+geovanitec@users.noreply.github.com>
2020-03-20 18:20:25 -0600

    ACTUALIZACION DE TABLAS

---
+++ INSERTS.sql
@@ -0,0 +1,5 @@
+
+Insert into SEDE values ("001" , "PORTALES" , "zona17");
+Insert into SEDE values ("002" , "SANJOSE" , "CARRETERA_AL_SALVADOR");
+Insert into SEDE values ("003" , "CENTRAL" , "zona2");
+
--- SISTEMA_UNIVERSIDAD.sql
+++ SISTEMA_UNIVERSIDAD.sql
@@ -1,12 +1,51 @@
 create database SISTEMA_UMG;
 use SISTEMA_UMG;
 
-create table BOLETAS(
-NumeroBoleta int primary key,
-CodigoAlumno int,
-Mes varchar(20) not null, 
-Ciclo int,
-Monto float
+create table SEDE
+(
+
+CodigoSede int primary key,
+NombreSede varchar (70) not null,
+DireccionSede varchar (100) not null
+
+)Engine = InnoDB Default charset = Latin1 ;
+
+create table CURSOS
+(
+CodigoCurso int primary Key,
+NombreCurso varchar(70) not null
+
+)Engine = InnoDB Default charset = Latin1;
+
+create table CATEDRATICOS
+(
+CodigoCatedratico int primary key,
+NombreCatedratico varchar (70) not null,
+CursoCatedratico varchar (70) not null,
+CorreoCatedratico varchar (100) not null
+ #foreign key (CodigoCurso) references CURSOS(CodigoCurso)
+)Engine = InnoDB Default charset = Latin1 ;
+
+create table FACULTADES
+(
+CodigoFacultad int primary key,
+NombreFacultad varchar (70)not null,
+CodigoSede int not null,
+#CodigoCurso int not null,
+foreign key (CodigoSede) references SEDE(CodigoSede)
+#foreign key (CodigoCurso) references CURSO(CodigoCurso)
+)Engine = InnoDB Default charset = Latin1 ;
+
+create table HORARIOS
+(
+NumeroHorario int primary key,
+IdCatedratico int not null,
+IdFacultad int not null,
+IdCurso int not null,
+foreign key(IdCurso) references CURSOS(CodigoCurso),
+foreign key(IdCatedratico) references CATEDRATICOS (CodigoCatedratico),
+foreign key(IDFacultad)references FACULTADES(CodigoFacultad)
+
 )Engine = InnoDB Default charset = Latin1 ;
 
 create table ALUMNOS 
@@ -15,49 +54,31 @@ NumeroCarnet int primary Key,
 NombreAlumno varchar(70) not null,
 CorreoAlumno varchar (100) not null,
 CarreraAlumno varchar (100) not null,
-CicloAlumno int not null
-
+CicloAlumno int not null,
+HorarioAlumno int not null,
+IdAlumno int not null,
+IdCurso int not null,
+foreign key(IdCurso) references CURSOS(CodigoCurso),
+foreign key (IdAlumno) references SEDE (CodigoSede),
+foreign key (HorarioAlumno) references HORARIOS (NumeroHorario)
 )Engine = InnoDB Default charset = Latin1 ;
 
-create table CATEDRATICOS
+create table ADMINISTRACION 
 (
-CodigoCatedratico int primary key,
-NombreCatedratico varchar (70) not null,
-CursoCatedratico varchar (70) not null,
-CorreoCatedratico varchar (100) not null
+CodigoAdministracion int primary key,
+PagosMesColegiatura Varchar(20) not null,
+MontoPago float not null,
+MultasPago float not null,
+IdAlumno int not null,
+IdSede int not null,
+IdFacultad int not null,
+IdHorario int not null,
+IdCurso int not null,
+foreign key(IdCurso) references CURSOS(CodigoCurso),
+foreign key(IdAlumno) references ALUMNOS (NumeroCarnet),
+foreign key(IdSede) references SEDE (CodigoSede),
+foreign key(IdFacultad)references FACULTADES(CodigoFacultad),
+foreign key(IdHorario) references HORARIOS (NumeroHorario)
+
+
 )Engine = InnoDB Default charset = Latin1 ;
-
-create table ALUMNOS_SEDE
-(
-NumeroCarnet int primary Key,
-NombreAlumno varchar(70) not null,
-CorreoAlumno varchar (100) not null,
-CarreraAlumno varchar (100) not null,
-CicloAlumno int not null
-
-)Engine = InnoDB Default charset = Latin1 ;
-
-create table CATEDRATICOS_SEDE
-(
-CodigoCatedratico int primary key,
-NombreCatedratico varchar (70) not null,
-CursoCatedratico varchar (70) not null,
-CorreoCatedratico varchar (100) not null
-)Engine = InnoDB Default charset = Latin1 ;
-
-
-
-create table FACULTADES
-(
-CodigoFacultad int primary key,
-NombreFacultad varchar (70)not null
-)Engine = InnoDB Default charset = Latin1 ;
-
-
-create table CONTROL_CATEDRATICO
-(
-foreign key(NumeroCarnet)references ALUMNOS (NumeroCarnet)
-
-)
-
-
